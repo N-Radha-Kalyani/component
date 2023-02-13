82f0f991719f46042dee08e16e0ef92a2e95c278
radha <radhakalyanina@gmail.com>
2023-02-13 06:21:08 +0530

    initial commit

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div><p>This app uses cookies</p></div>',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+
+}
